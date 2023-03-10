a416c9119a3337ba16abb822edf56197e8fa54c4
&midnight <106489765+TheMidnightShow@users.noreply.github.com>
2023-03-09 21:12:59 -0600

    Update main.cpp

--- clock/clock/main.cpp
+++ clock/clock/main.cpp
@@ -1,29 +1,5 @@
 #include "clock.hpp"
 
-void set_time(std::string& time)
-{
-  std::time_t time_stamp  = std::time(nullptr);
-  std::string time_string = std::asctime(std::localtime(&time_stamp));
-
-  for(int i = 0; i < time_string.size(); i++)
-  {
-    if(time_string[i+2] == ':')
-    {
-      time.push_back(time_string[i]);
-      time.push_back(time_string[i+1]);
-
-      time.push_back(time_string[i+3]);
-      time.push_back(time_string[i+4]);
-
-      time.push_back(time_string[i+6]);
-      time.push_back(time_string[i+7]);
-
-      break;
-    }
-  }
-}
-
-
 int main()
 {
   Clock my_clock;
